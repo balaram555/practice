697d14e2362b0c53bfd5d4b0036fb7720d25a103
Balaram <22a91a0599@aec.edu.in>
2024-10-18 11:20:00 +0530

    odd check

---
+++ odd.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int i=0,j=0;
+  while(i<5)
+{
+  cout<<"*"<<endl;
+  j++;
+  if(j==4)
+  {
+    i++;
+    cout<<""<<endl;
+  }
+}    
+}
